dc6dd880a2a58afb0ad19154c9835e024633569d
Nutthakit Anansuksakul <ing118199@gmail.com>
2021-08-21 23:26:17 +0700

    folder separate

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <template>
-  <div id="nav">
+  <div>
+    <!-- router-link เหมือน href -->
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/about">About</router-link> -->
+    <!-- การเรียกผ่านช่ือ ถ้ามีการเปลี่ยน url ก็ยังทำงานได้เพราะใช้ name 
+        ใส่ : คือ blind to 
+    -->
+
+    <router-link :to="{ name: 'About' }">About</router-link>
   </div>
-  <router-view/>
+  <router-view/>    <!-- ไว้โหลดข้อมูล -->
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+//ใส่ @ จะไปเร่ิมที่ src แล้หาไฟล์ให้เอง
+import About from '@/views/About.vue'
 
 const routes = [
   {
@@ -10,11 +12,8 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: About
+  },
 ]
 
 const router = createRouter({
