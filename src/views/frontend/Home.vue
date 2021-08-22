7abc740f90d31b3b846fa6a9c92c49a697d8f076
Nutthakit Anansuksakul <ing118199@gmail.com>
2021-08-22 21:12:08 +0700

    installed tailwind

--- src/views/frontend/Home.vue
+++ src/views/frontend/Home.vue
@@ -1,4 +1,6 @@
 <template>
+<div class="flex items-center justify-center h-full bg-blue-500">  
   <h1>Home Page</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse, explicabo minima unde ipsam cupiditate nihil temporibus minus eveniet tenetur, nam sequi asperiores nisi adipisci iusto quam modi eius, rerum porro est facere blanditiis recusandae. Eius nulla error illo sint nobis atque, ea eaque sequi sed sunt ipsam hic fugiat.</p>
+</div>
 </template>
