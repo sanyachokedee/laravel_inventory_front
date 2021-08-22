716d03da0fa45fc010ad180ae9af46996180f8b1
Nutthakit Anansuksakul <ing118199@gmail.com>
2021-08-22 21:11:52 +0700

    installed tailwind

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/styles/tailwind.css'
 
-createApp(App).use(store).use(router).mount('#app')
+//สร้างตัวแปรมาเก็บ Constant ของ vue
+// createApp(App).use(store).use(router).mount('#app') เปลี่ยนเป็น
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.mount('#app')
+
