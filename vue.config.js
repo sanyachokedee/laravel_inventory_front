3c962f8687a34bbe494ef384429e1a4bcd50dabd
sanyachokedee <69395545+sanyachokedee@users.noreply.github.com>
2021-08-25 08:42:58 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     //กรณี build ถ้า host จริงมักไม่มีปัญหา แต่ถ้าเป็นจำลอง ต้องระะบุชื่อ Folder ด้วย จะมีผลในการลิงค์ไฟล์จากโฟลเดอร์ต่างๆ
-    publicPath: process.env.NODE_ENV === 'production' ? '/demovuejs' : '/'
-    // publicPath: process.env.NODE_ENV === 'production' ? '/': '/'
+    //publicPath: process.env.NODE_ENV === 'production' ? '/demovuejs' : '/'
+     publicPath: process.env.NODE_ENV === 'production' ? '/': '/'
 }
