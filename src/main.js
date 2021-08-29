68d9caafcba564cf3e70ce76b3182266bb94f321
Nutthakit Anansuksakul <ing118199@gmail.com>
2021-08-29 20:56:58 +0700

    day6-part3

--- src/main.js
+++ src/main.js
@@ -2,12 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// import tailwind
 import '@/styles/tailwind.css'
+
+// import sweetalert2
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 
 //สร้างตัวแปรมาเก็บ Constant ของ vue
 // createApp(App).use(store).use(router).mount('#app') เปลี่ยนเป็น
 const app = createApp(App)
 app.use(store)
 app.use(router)
-app.mount('#app')
 
+app.use(VueSweetalert2);
+app.mount('#app')
